cafc147743a6871acace54b605a5c1e27959ea32
pabhilashlearn <103013922+pabhilashlearn@users.noreply.github.com>
2022-04-29 12:34:41 -0400

    Moving project with .travis.yml to root

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacityuser
+export POSTGRES_PASSWORD=udacityuser
+export POSTGRES_HOST=udacity-project-postgresql.caa7fxoi5e0j.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-354396412510-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
